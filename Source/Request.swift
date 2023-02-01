e98a79d8246bfd4a3c99e40ad5ee1e741b671db8
Fredrik Palm <fredrik.palm@appva.com>
2023-02-01 10:35:20 +0100

    Removed comment

--- Source/Request.swift
+++ Source/Request.swift
@@ -112,7 +112,6 @@ public class Request {
         var tasks: [URLSessionTask] = []
         /// All `URLSessionTaskMetrics` values gathered by Alamofire on behalf of the `Request`. Should correspond
         /// exactly the the `tasks` created.
-        /// private var _selectionFeedbackGenerator: Any? = nil
         private var _metrics: [Any] = []
         @available(iOS 10.0, *)
         fileprivate var metrics: [URLSessionTaskMetrics] {
